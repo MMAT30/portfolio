bead1af6e0ea1ac2c9c7fc1c3f58e2d2f7a80042
Mateo Martinez <mateo.martinez.spc@icloud.com>
2022-11-13 20:23:36 -0600

    creted routes and links in header

---
+++ src/components/Routes/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container-fluid bg-light">
+      <div className="row justify-content-center">
+        <div className="col-auto">
+            <p className="lead">
+                About
+            </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
